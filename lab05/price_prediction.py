3b364d0786b892cb28c1840c6cd6138ecc12c010
slendchat <slendchat@gmail.com>
2025-11-06 16:25:00 +0200

    lab05 initial commit

---
+++ lab05/price_prediction.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+def fuzzy_memebership(value, low, mid, high) -> tendention:
+  ...
+
+class tendention(Enum):
+  low = "low"
+  mid = "mid"
+  high = "high"
+
+class demand:
+  def __init__(self, value):
+    self.value = value
+
+class supply:
+  def __init__(self, value):
+    self.value = value
+
+class inflation:
+  def __init__(self, value):
+    self.value = value
+
